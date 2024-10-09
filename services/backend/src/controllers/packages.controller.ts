306b0cd5374d7564e40824ad5be771716f9e676d
Kuhle Hanisi <hanisikuhle@gmail.com>
2024-10-09 21:19:52 +0200

    Update packages.controller.ts

--- services/backend/src/controllers/packages.controller.ts
+++ services/backend/src/controllers/packages.controller.ts
@@ -3,13 +3,27 @@ import Package from '../db/models/Package';
 
 export const getAllPackages = async (_req: Request, res: Response) => {
 	try {
+		const type = 'local';
+
 		const packages = await Package.findAll({
-			where: {},
+			// where: {
+			// 	type,
+			// },
+			include: [
+				{
+					association: 'operator',
+					attributes: ['title', 'type', 'esim_type', 'id'],
+					where: {
+						type,
+					},
+				},
+			],
 			order: [
 				['price', 'ASC'],
 				['amount', 'DESC'],
 				['day', 'DESC'],
 			],
+			limit: 2,
 		});
 
 		// const packagesWithPrices = packages.map((pkg) => {
